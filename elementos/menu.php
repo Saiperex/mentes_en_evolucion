40fc3aabff1a215afb952495c639cde3cc501d66
Xavier <xaviramos668@gmail.com>
2024-02-03 21:52:00 -0300

    comenzando proyecto

---
+++ elementos/menu.php
@@ -0,0 +1,56 @@
+<div class="slide_button">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+</div>
+<nav class="cabecera">
+    <a class="logo" href="index.php"><img src="img/recursos/logo2.png" alt="logo Mentes_En_Evolucion"></a>
+    <ul>
+        <div class="buscador">
+            <form action="" method="post">
+                <input type="text" name="buscador" id="buscador">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+        <li><a href="Cursos">Cursos</a></li>
+        <li><a href="Capacitaciones">Capacitaciones</a></li>
+        <li><a href="Planes">Planes</a></li>
+        <li><a href="blog">blog</a></li>
+        <li class="login"><a href="Iniciar Sesión">Iniciar Sesión</a></li>
+    </ul>
+</nav>
+<script>
+  const slideButton = document.querySelector('.slide_button');
+  const header = document.querySelector('header');
+
+  slideButton.addEventListener('click', () => {
+    // Cambiar las líneas para formar una X
+    slideButton.classList.toggle('activado');
+    validador();
+  });
+
+  function validador() {
+    if (slideButton.classList.contains('activado')) { // Corrección aquí
+        header.style.left = '0';
+    } else {
+        header.style.left = '-100%';
+    }
+  }
+
+  // Llama a validador() para establecer la posición inicial al cargar la página
+  validador();
+  function verificarAncho() {
+    const anchoPantalla = window.innerWidth;
+
+    if (anchoPantalla > 1145) {
+      // Haz algo cuando el ancho de la pantalla sea mayor a 1145 pixeles
+      header.style.left = '0';
+      // Puedes agregar aquí más acciones o llamadas a funciones según tus necesidades
+    }
+  }
+  // Llama a verificarAncho al cargar la página
+  verificarAncho();
+
+  // Agrega un evento de cambio de tamaño de ventana
+  window.addEventListener('resize', verificarAncho);
+</script>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--ICONS-->
+    <script src="https://kit.fontawesome.com/c8bf7962f3.js" crossorigin="anonymous"></script>
+    <!--FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Open+Sans:wght@300&family=Raleway:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <!--CSS-->
+    <link rel="stylesheet" href="css/init.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <!--META-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MENTES EN EVOLUCION - PREPARANDOTE PARA EL FUTURO</title>
+</head>
+<body>
+    <header>
+        <?php include ("elementos/menu.php")?>
+    </header>
+    <main>
+        <section class="seccion inicio">
+            <div class="seccion_centrado">
+                <div class="inicio_izq">
+                    <h1>¡Explora tu potencial!</h1>
+                    <p>Descubre cursos y capacitaciones que impulsarán tu crecimiento en Mentes en Evolución.</p>
+                    <div class="conteiner_cards_inicio">
+                        <div class="card_inicio">
+                            <i class="graduation fas fa-graduation-cap"></i>
+                            <h2>Cursos Variados</h2>
+                            <p>Explora nuestra amplia variedad de cursos para impulsar tu aprendizaje en diferentes áreas.</p>
+                        </div>
+                        <div class="card_inicio">
+                            <i class="teacher fas fa-chalkboard-teacher"></i>
+                                <h2>Profesores Expertos</h2>
+                                <p>Aprende de profesionales altamente calificados que guiarán tu camino hacia el conocimiento.</p>
+                        </div>
+                        <div class="card_inicio">
+                            <i class="globe fas fa-globe"></i>
+                            <h2>Acceso Global</h2>
+                            <p>Disfruta de nuestros cursos desde cualquier lugar del mundo, sin límites geográficos.</p>
+                        </div>
+                        <div class=" card_inicio">
+                            <i class="clock fas fa-clock"></i>
+                            <h2>Aprendizaje Flexible</h2>
+                            <p>Adapta tus horarios con nuestros cursos flexibles diseñados para tu comodidad.</p>
+                        </div>
+                        <div class="card_inicio">
+                            <i class="users fas fa-users"></i>
+                            <h2>Comunidad Activa</h2>
+                            <p>Únete a una vibrante comunidad de estudiantes y profesionales compartiendo conocimientos.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="inicio_der">
+                    <object data="img/recursos/inicio.svg" type="image/svg+xml"></object>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
